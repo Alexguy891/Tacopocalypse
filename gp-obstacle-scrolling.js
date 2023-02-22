290e9b4d7e5565a854893f3b499e34e578444c34
Nicholas Moore <nmoore22@kent.edu>
2023-02-22 17:24:01 -0500

    fixed comments

--- gp-obstacle-scrolling.js
+++ gp-obstacle-scrolling.js
@@ -1,50 +1,46 @@
-// Where is the circle
+// Variables
 let x, y, a, b, c, d, rx, ra, rc;
 
 function setup() {
-  createCanvas(720, 400);
-  // Starts in the middle
-  x = width;
-  y = height/2;
-  a = width;
-  b = height/4;
-  c = width;
-  d = height/1.5;
-  rx = random(2);
-  ra = random(2);
-  rc = random(5);
+    createCanvas(720, 400);
+    // Starting location of rectangles
+    x = width;
+    y = height / 2;
+    a = width;
+    b = height / 4;
+    c = width;
+    d = height / 1.5;
+    rx = random(5);
+    ra = random(5);
+    rc = random(5);
 }
 
 function draw() {
-  background(200);
+    background(200);
 
-  // Draw a circle
-  stroke(50);
-  fill(100);
-  rect(x, y, 120, 40);
-  rect(a, b, 120, 40);
-  rect(c, d, 120, 40);
+    // Draw rectangles
+    stroke(50);
+    fill(100);
+    rect(x, y, 120, 40);
+    rect(a, b, 120, 40);
+    rect(c, d, 120, 40);
 
-  // Jiggling randomly on the horizontal axis
-  y = y;
-  b = b;
-  d = d;
-  // Moving up at a constant speed
-  x = x - rx;
-  a = a - ra;
-  c = c - rc;
+    // Speed
+    x = x - rx;
+    a = a - ra;
+    c = c - rc;
 
-  // Reset to the bottom
-  if (x < -120) {
-    rx = random(5);
-    x = width;
-  }
-  if (a < -120) {
-    ra = random(5);
-    a = width;
-  }
-  if (c < -120) {
-    rc = random(5);
-    c = width;
-  }
+    // Reset to the right side of screen with a random speed
+    if (x < -120) {
+        rx = random(5);
+        x = width;
+    }
+    if (a < -120) {
+        ra = random(5);
+        a = width;
+    }
+    if (c < -120) {
+        rc = random(5);
+        c = width;
+    }
 }
