9437a9d8ea2360c85f2753a6e02b7ba7b72ed36b
Alex Ely <ely.m.alexander@gmail.com>
2023-02-24 15:10:50 -0500

    Added runner movement

--- runner.js
+++ runner.js
@@ -1,12 +1,32 @@
-function Runner(y) {
+function Runner(y, w, h) {
     this.x = 0;
     this.y = y;
     this.w = 50;
     this.h = 50;
-
+    this.velocity = 0;
+    this.gravity = 5;
+  
     this.show = function() {
         fill(100);
         stroke(50);
         rect(this.x, this.y, this.w, this.h)
+        
+        if(this.y + this.h < h) {
+          this.y += this.gravity;
+        }  
+      
+        this.y += this.velocity;
+        this.velocity /= 1.2;
+    
+        if(keyIsDown(RIGHT_ARROW) && this.x < w - this.w) {
+            this.x += 3;
+        } else if(keyIsDown(LEFT_ARROW) && this.x > 0) {
+            this.x -= 3;
+        }
+        
+        if(keyIsDown(UP_ARROW) && this.y + this.h >= h) {
+            this.velocity = -25;
+            keyCode = DOWN_ARROW;
+        }
     }
 }
