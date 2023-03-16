c7af3e3eef3040ed5152ecbaca8edceeef5cf6be
JackHeine <117319871+JackHeine@users.noreply.github.com>
2023-03-16 15:56:42 -0400

    update app.js to be complient with function call rules declared inside obstacle.js and runner.js.
    should have been done earlier but i butchered a commit. meant to commit a local branch i made that extended main, accidentally committed to main.

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 function setup() {
     createCanvas(720, 400);
-    runner = new Runner(height - 50);
-    obstacleA = new Obstacle(width, height);
-    obstacleB = new Obstacle(width, height / 2);
-    obstacleC = new Obstacle(width, height / 4);
-    print("hello")
+  
+    runner = new RunnerEntity(50, 50, 50, 50, 50, 50, 'assets/waffle.jpg', 3);
+    obstacleA = new ObstacleEntity(width  , height - 10, 50, 25, 50, 25, 'assets/waffle2.jpg', true, true, 3, 1);
+    obstacleB = new ObstacleEntity(width + 200 , height -20, 50, 25, 50, 25, 'assets/waffle2.jpg', true, true, 2, 1);     
+    obstacleC = new ObstacleEntity(width + 300 , height - 5, 50, 25, 50, 25, 'assets/waffle2.jpg', true, true, 5, 1);   
 }
 
 function draw() {
-    background(220);
-    runner.show();
+    background(200);
+    runner.runnerShow();
     obstacleA.show();
-    obstacleB.show();
-    obstacleC.show();
-    print("hello")
+  for(var i = 0; i < ObstacleList.length; i++){
+    ObstacleList[i].show();
+    runner.collision(ObstacleList[i])
+  }
+  runner.collision(obstacleA);
+    //obstacleB.show();
+    //obstacleC.show();
 }
