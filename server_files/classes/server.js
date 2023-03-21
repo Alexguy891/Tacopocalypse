316203d80f550aa1662d2bac631965714b37e142
Kojavoh <114598688+Kojavoh@users.noreply.github.com>
2023-03-21 00:18:13 +0000

    Updating with current version

--- server_files/classes/server.js
+++ server_files/classes/server.js
@@ -1,73 +1,120 @@
 class Ingredient extends Entity {
-  constructor (posX, posY, collisionHeight, collisionWidth, sizeHeight, sizeWidth, spritePath, name, cookingTime) {
-      super(posX, posY, collisionHeight, collisionWidth, sizeHeight, sizeWidth, spritePath, false, true);
+    constructor (posX, posY, collisionHeight, collisionWidth, sizeHeight, sizeWidth, spritePath, name, cookingTime) {
+        super(posX, posY, collisionHeight, collisionWidth, sizeHeight, sizeWidth, spritePath, false, true);
 
-      // ingredient name
-      this.name = name;
+        // ingredient name
+        this.name = name;
 
-      // time to cook
-      this.cookingTime = cookingTime;
+        // time to cook
+        this.cookingTime = cookingTime;
 
-      // for cooked checking
-      this.cooked = false;
+        // array for ingredient names within TacoShell ingredient objects
+        this.ingredientStack = [];
+        if(this.name == "TacoShell") {
+            this.ingredientStack.push(this.name);
+        }
 
-      // for dragging
-      this.isDragged = false;
-      this.mouseOffsetX = 0;
-      this.mouseOffsetY = 0;
-  }
+        // for cooked checking
+        this.cooked = false;
 
-  // displays ingredient
-  show() {
-    image(this.spriteImage, this.positionArray[0], this.positionArray[1], this.sizeArray[0], this.sizeArray[1]);
-  }
+        // for dragging
+        this.isDragged = false;
+        this.mouseOffsetArray = [0, 0];
 
-  // check if ingredient clicked
-  clicked() {
-      // check if clicked
-      if (mouseX > this.positionArray[0] && mouseX < this.positionArray[0] + this.collisionArray[1] 
-          && mouseY > this.positionArray[1] && mouseY < this.positionArray[1] + this.collisionArray[0]) {
-          this.isDragged = true;
+        // for returning to previous position
+        this.prevPositionArray = [0, 0];
+    }
 
-          // track dragging coords
-          this.mouseOffsetX = this.positionArray[0] - mouseX;
-          this.mouseOffsetY = this.positionArray[1] - mouseY;
-      }
-  }
+    // displays ingredient
+    show() {
+        image(this.spriteImage, this.positionArray[0], this.positionArray[1], this.sizeArray[0], this.sizeArray[1]);
+    }
 
-  // reset drag variable after release
-  release() {
-      this.isDragged = false;
-  }
+    // check if ingredient clicked
+    clicked() {
+        // check if clicked
+        if (mouseX > this.positionArray[0] && mouseX < this.positionArray[0] + this.collisionArray[1] 
+            && mouseY > this.positionArray[1] && mouseY < this.positionArray[1] + this.collisionArray[0]) {
+              this.isDragged = true;
+              this.prevPositionArray[0] = this.positionArray[0];
+              this.prevPositionArray[1] = this.positionArray[1];
 
-  // update position when dragged
-  update() {
-      if(this.isDragged) {
-          this.positionArray[0] = mouseX + this.mouseOffsetX;
-          this.positionArray[1] = mouseY + this.mouseOffsetY;
-      }
-  }
+              // track dragging coords
+              this.mouseOffsetX = this.positionArray[0] - mouseX;
+              this.mouseOffsetY = this.positionArray[1] - mouseY;
+        }
+    }
+
+    // reset drag variable after release
+    release() {
+        let ret = false;
+        if(isDragged) {
+            ret = true;
+        }
+        this.isDragged = false;
+        return ret;
+    }
+
+    // update position when dragged
+    update() {
+        if(this.isDragged) {
+          this.positionArray[0] = mouseX + this.mouseOffsetArray[0];
+          this.positionArray[1] = mouseY + this.mouseOffsetArray[1];
+        }
+    }
+
+    isStackable(ingredient) {
+        if(this.name != "Taco Shell") 
+            return false;
+        ingredientStack.forEach(it => {
+            if(it == ingredient.name) {
+                return false;
+            }
+        });
+        return true;
+    }
+
+    addIngredient(ingredient) {
+        if(this.isStackable(ingredient)) {
+            this.ingredientStack.push(ingredient.name);
+            return true;
+            // Call somewhere to obliterate added ingredient object
+        }
+        return false;
+    }
+
+    isColliding(ingredient) {
+        if (mouseX > ingredient.positionArray[0] && mouseX < ingredient.positionArray[0] + ingredient.collisionArray[1] 
+            && mouseY > ingredient.positionArray[1] && mouseY < ingredient.positionArray[1] + ingredient.collisionArray[0]) {
+            return true;
+        }
+        return false;
+    }
+
+    bounce() {
+        this.positionArray[0] = this.prevPositionArray[0];
+        this.positionArray[1] = this.prevPositionArray[1];
+    }
 }
 
-// define images
-// let tacoShellImg, tacoMeatImg, tomatoImg, cheeseImg, hotSauceImg;
-// let canvasX = 500, canvasY = 500;
-// let interactableArray = [];
 
-// define starting positions of images
+
+
+
+// // define starting positions of images
 // const startPositions = [
-//   { image: 'tacoShellImg', x: 10, y: 10, width: 50, height: 50, ingredient: Ingredient, cookingTime: -1 },
-//   { image: 'tacoMeatImg', x: 60, y: 10, width: 50, height: 50, ingredient: Ingredient, cookingTime: 3 },
-//   { image: 'tomatoImg', x: 110, y: 10, width: 50, height: 50, ingredient: Ingredient, cookingTime: -1 },
-//   { image: 'cheeseImg', x: 160, y: 10, width: 50, height: 50, ingredient: Ingredient, cookingTime: -1 },
-//   { image: 'hotSauceImg', x: 210, y: 10, width: 50, height: 50, ingredient: Ingredient, cookingTime: -1 }
-// ];
-
-// // define target area for each type of ingredient
-// const targetAreas = [
-//   { image: 'tacoShellImg', x: 300, y: 10 },
-//   { image: 'tacoMeatImg', x: 300, y: 80 },
-//   { image: 'tomatoImg', x: 300, y: 150 },
-//   { image: 'cheeseImg', x: 300, y: 220 },
-//   { image: 'hotSauceImg', x: 300, y: 290 }
-// ];
+//     { image: 'tacoShellImg', x: 10, y: 10, width: 50, height: 50, ingredient: Ingredient, cookingTime: -1 },
+//     { image: 'tacoMeatImg', x: 60, y: 10, width: 50, height: 50, ingredient: Ingredient, cookingTime: 3 },
+//     { image: 'tomatoImg', x: 110, y: 10, width: 50, height: 50, ingredient: Ingredient, cookingTime: -1 },
+//     { image: 'cheeseImg', x: 160, y: 10, width: 50, height: 50, ingredient: Ingredient, cookingTime: -1 },
+//     { image: 'hotSauceImg', x: 210, y: 10, width: 50, height: 50, ingredient: Ingredient, cookingTime: -1 }
+//   ];
+  
+//   // define target area for each type of ingredient
+//   const targetAreas = [
+//     { image: 'tacoShellImg', x: 300, y: 10 },
+//     { image: 'tacoMeatImg', x: 300, y: 80 },
+//     { image: 'tomatoImg', x: 300, y: 150 },
+//     { image: 'cheeseImg', x: 300, y: 220 },
+//     { image: 'hotSauceImg', x: 300, y: 290 }
+//   ];
--- test_server.js
+++ test_server.js
@@ -9,6 +9,9 @@ INGREDIENT_SPRITEPATH = "server_files/assets/Ground_Beef.png";
 INGREDIENT_NAME = "ground_beef" 
 INGREDIENT_COOKINGTIME = 5;
 
+// ingredient-tracking stack
+let interactableStack = [];
+
 function setup() {
     createCanvas(720, 400);
     
@@ -16,26 +19,93 @@ function setup() {
     ingredient = new Ingredient(INGREDIENT_POSX, INGREDIENT_POSY, INGREDIENT_COLLISIONHEIGHT, 
         INGREDIENT_COLLISIONWIDTH, INGREDIENT_SIZEHEIGHT, INGREDIENT_SIZEWIDTH,
         INGREDIENT_SPRITEPATH, INGREDIENT_NAME, INGREDIENT_COOKINGTIME);
+
+    interactableStack.push(ingredient);
 }
 
 function draw() {
     background(220);
+    
+    // For every ingredient object in the scene...
+    interactableStack.forEach(it1 => {
+        // update if being dragged
+        it1.update();
 
-    // update if being dragged
-    ingredient.update();
-
-    // show ingredient
-    ingredient.show();
+        // show ingredient
+        it1.show();
+    });
 }
 
 // runs when mouse pressed anywhere
 function mousePressed() {
-    // checks for mouse click
-    ingredient.clicked();
-}
+    // For every ingredient object in the scene...
+    interactableStack.forEach(it1 => {
+        // checks for mouse click
+        it1.clicked();
+    });
+} 
 
 // runs when mouse released anywhere
 function mouseReleased() {
-    // checks for mouse release
-    ingredient.release();
+    // For every ingredient object in the scene...
+    interactableStack.forEach(it1 => {
+        // Checks which ingredient was dropped
+        if(it1.release()) {
+            // Check if ingredient was dropped on another ingredient
+            //interactableStack.forEach(it2 => {
+            for(let i = 0; i < interactableStack.length; ++i) {
+                // Check if ingredient isColliding with dropped ingredient
+                if(it1.isColliding(interactableStack[i])) {
+                    // If true, try calling addIngredient, which returns a bool;
+                    // returns true if ingredient was successfully added, false otherwise
+                    if(it1.addIngredient(interactableStack[i])) {
+                        // If true, ingredient gets added to found ingredient and we
+                        // obliterate added ingredient
+                        interactableStack = interactableStack.splice(i, 1);
+                    }
+                    else {
+                        // If false, bounce ingredient back to previous position
+                        it1.bounce();
+                    }
+                }
+                else {
+                    // If false, bounce ingredient back to previous position
+                    it1.bounce();
+                }
+            });
+        }
+
+
+
+
+        /*
+        if(it1.release()) {
+            // Check if ingredient was dropped on another ingredient
+            let collisionStack = [];
+            interactableStack.forEach(it2 => {
+                if(it2 != it1) {
+                    let y = it1.positionArray[1] - it2.positionArray[1];
+                    let x = it1.positionArray[0] - it2.positionArray[0];
+
+                    let distance = Math.sqrt(x * x + y * y);
+                    collisionStack.push(distance);
+                }
+                else {
+                    collisionStack.push(-1);
+                }
+            });
+            let minDistance = 999999;
+            let minIndex = -1;
+            for(let i = 0; i < collisionStack.length; ++i) {
+                if(collisionStack[i] < minDistance && collisionStack[i] != -1) {
+                    minDistance = collisionStack[i];
+                    minIndex = interactableStack[i];
+                }
+            }
+            // if so, call addIngredient
+            // if true, ingredient gets added to found ingredient and obliterate added ingredient
+            // if false, bounce ingredient back to previous position
+        }
+        */
+    });
 }
