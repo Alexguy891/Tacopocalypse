56e99fe2a92f72145b037cf1eee2d85adc9e9c39
Nicholas Moore <nmoore22@kent.edu>
2023-04-11 16:05:53 -0400

    added obstacle standing

--- runner_files/classes/runner.js
+++ runner_files/classes/runner.js
@@ -18,6 +18,9 @@ class RunnerEntity extends Entity {
 
     // loss of lives
     collision(ObstacleEntity) {
+        if (ObstacleEntity.lifeDamage == 0) { // Platforms do not damage player
+          this.isStandable(ObstacleEntity);
+        }
         if (
           (ObstacleEntity.positionArray[0] + ObstacleEntity.collisionArray[0] >= this.positionArray[0]) &&
 (ObstacleEntity.positionArray[0] <= this.positionArray[0] + this.collisionArray[0]) &&
@@ -32,6 +35,26 @@ class RunnerEntity extends Entity {
           print("player death")
         }
     }
+
+
+    // if runner's x coordinate is in between of a standable object
+    // Whenever runner's y coordinate should not be less than the y coordinate of the obstacle if it is standable
+
+    // PLATFORM POSITIONARRAY[0] changes as it approaches the player
+    // ''       ''           [1] stays at 380
+    // ''       BOTH COLLISION ARRAYS PRINT 20 constantly
+    // RUNNER POSITION ARRAY [0] == 0 [1] == 380 but changes on jump
+    // RUNNER POSITION ARRAY[1] would need updated when runner is in between of the platform object
+    // Left side of object is positionArray[0] right side is positionArray[0] plus [1] ?
+    isStandable(ObstacleEntity) {
+      if (this.positionArray[0] > ObstacleEntity.positionArray[0] &&
+          this.positionArray[0] < ObstacleEntity.positionArray[0] + ObstacleEntity.positionArray[1]) { // checks that runner's x coord is within a platforms x coord
+            if (this.positionArray[1] < ObstacleEntity.positionArray[1]) { // checks runner's y coord against platforms y coord
+              this.positionArray[1] = ObstacleEntity.positionArray[1] - ObstacleEntity.collisionArray[1];
+            }
+      }
+    }
+
     
     show() {
       // show image
